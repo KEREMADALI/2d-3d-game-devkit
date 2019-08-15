bc73de53b0625749501c8853510892b5d9dc8b3c
Kerem Adalı <kerem.adalik@gmail.com>
2019-08-16 00:56:19 +0300

    Movement Animation & Scripts

---
+++ Assets/CharacterMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : Movement
+{
+    #region Private & Const Variables
+
+    #endregion
+
+    #region Public Variables
+
+    #endregion
+
+    #region Public Methods
+
+    #endregion
+
+    #region Private Methods
+    private void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+
+        UpdateAnimation();
+    }
+
+    #endregion
+}
