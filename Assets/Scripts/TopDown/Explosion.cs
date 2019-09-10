88b262bd5f9bc2139a37dbc956c45864fafa6972
Kerem Adalı <kerem.adalik@gmail.com>
2019-09-10 23:01:03 +0300

    2D explosion handler

---
+++ Assets/Scripts/TopDown/Explosion.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    #region Private & Const Variables
+
+    #endregion
+
+    #region Public Variables
+
+    #endregion
+
+    #region Public Methods
+
+    public static void CreateExplosion(Vector2 explosionCenter, float power, float radius)
+    {
+        Collider2D[] hitColliders = Physics2D.OverlapCircleAll(explosionCenter, radius);
+
+        for ( int i=0; i < hitColliders.Length  ; i++ )
+        {
+            ApplyForce(explosionCenter, hitColliders[i].attachedRigidbody, power);
+        }
+    }
+
+    private static void ApplyForce(Vector2 explosionCenter, Rigidbody2D rigidbody, float power)
+    {
+        var direction = ((Vector2)rigidbody.position - explosionCenter).normalized;
+        Vector2 forceVector = direction * power;
+
+        rigidbody.AddForce(forceVector);
+    }
+
+
+    #endregion
+
+    #region Private Methods
+
+    #endregion
+}
---
+++ Assets/Scripts/TopDown/MainController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainController : MonoBehaviour
+{
+    #region Private & Const Variables
+
+    #endregion
+
+    #region Public Variables
+
+    #endregion
+
+    #region Public Methods
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Explosion.CreateExplosion(transform.position, 50, 5);
+        }
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    #endregion
+}
